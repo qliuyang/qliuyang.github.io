0438f82141b79bdf9cbd200f45777d0d5264b205
qliuyang <ly599575461@outlook.com>
2024-06-27 18:10:20 +0800

    update

---
+++ assets/ToolsView-BDwvsuoe.js
@@ -0,0 +1,69 @@
+import{b as d,d as G,o as B,c as l,u as s,a as e,t as u,_ as p,w as A,e as C,v as E,f as h,p as R,g as S,h as _,i as F,F as w,r as N,j as P}from"./index-B794u6D_.js";const U=["src","alt"],M={class:"content"},X={class:"title"},v=["innerHTML"],x={__name:"Game",props:["game"],setup(o){let n=o,t=d(n.game);G(()=>{t=n.game});function i(a){window.open(a)}return(a,r)=>(B(),l("main",null,[(B(),l("div",{key:s(t).id,class:"game"},[e("img",{class:"img",src:s(t).imgSrc,alt:s(t).title},null,8,U),e("div",M,[e("h2",X,u(s(t).title),1),e("p",{innerHTML:s(t).description,class:"description"},null,8,v),e("button",{onClick:r[0]||(r[0]=D=>i(s(t).downloadUrl)),class:"downloadBtn"}," 下载🧲 ")])]))]))}},T={class:"search"},I={__name:"Search",emits:["inputChange"],setup(o,{emit:n}){const t=n,i=d("");return A(i,a=>{t("inputChange",a)}),(a,r)=>(B(),l("main",null,[e("div",T,[C(e("input",{type:"text","onUpdate:modelValue":r[0]||(r[0]=D=>i.value=D),placeholder:"输入游戏名"},null,512),[[E,i.value]])])]))}},L=p(I,[["__scopeId","data-v-2680d5f7"]]),m=[{imgSrc:"/img/game/荒野大镖客2.jpg",title:"荒野大镖客2",description:`- 操作系统：需要 64 位处理器和操作系统 <br>
+- 处理器： Intel Core i5-2500K / AMD FX-6300 <br>
+- 内存： 8 GB RAM <br>
+- 显卡： 1Gb VRAM， DirectX 10+ <br>
+- 存储： 需要 119 GB
+`,downloadUrl:"magnet:?xt=urn:btih:B4927B4819B7CD7A33B16F14D159FEE9B130F8AD&tr=http%3A%2F%2Fbt3.t-ru.org%2Fann%3Fmagnet&dn=%5BDL%5D%20%5B%D0%9D%D0%B5%D0%BA%D0%BE%D0%BD%D0%B4%D0%B8%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D0%B0%D1%8F%20%D1%80%D0%B0%D0%B7%D0%B4%D0%B0%D1%87%D0%B0%5D%20Red%20Dead%20Redemption%20II%20(2)%20%5BP%5D%20%5BRUS%20%2B%20ENG%20%2B%2011%20%2F%20ENG%5D%20(2019%2C%20TPS)%20(1.0.1436.28)%20%5BP2P%5D"},{imgSrc:"/img/game/艾尔登法环.jpg",title:"艾尔登法环",description:`-操作系统 Windows 10 / 11 <br>
+-处理器：Intel Core i5-8400 / AMD Ryzen 3 3300X <br>
+-RAM内存：12 GB RAM <br>
+-显卡：GeForce GTX 1060 3Gb / AMD Radeon RX 580 4Gb，DirectX 12 <br>
+-存储：68 GB`,downloadUrl:"magnet:?xt=urn:btih:25139C1F6C77D7496E5B80A49D64F737F9A0B679&tr=http%3A%2F%2Fbt.t-ru.org%2Fann%3Fmagnet&dn=%5BDL%5D%20Elden%20Ring%20%5BP%5D%20%5BRUS%20%2B%20ENG%20%2B%2012%20%2F%20ENG%5D%20(2022%2C%20RPG)%20(1.12%20%2B%20DLC)%20%5BPortable%5D"},{imgSrc:"/img/game/极限竞速-地平线5.jpg",title:"极限竞速-地平线5",description:`- 需要 64 位处理器和操作系统 <br>
+- 操作系统： Windows 10 version 15063 and older <br>
+- 处理器： Intel i5-4460 / AMD Ryzen 3 1200 <br>
+- 内存： 8 GB RAM <br>
+- 显卡： NVidia GTX 970 / AMD RX 470， DirectX 12 <br>
+- 硬盘驱动器内存： 160 GB `,downloadUrl:"magnet:?xt=urn:btih:E18713187FC16C895F8EDAE3B0734389112C9000&tr=http%3A%2F%2Fbt3.t-ru.org%2Fann%3Fmagnet&dn=Forza%20Horizon%205%20%5BP%5D%20%5BRUS%20%2B%20ENG%20%2B%2014%20%2F%20ENG%20%2B%204%5D%20(2021)%20(1.576.537.0%20%2B%2046%20DLC)%20%5BScene%5D"},{imgSrc:"/img/game/纪元1800.jpg",title:"纪元1800",description:`操作系统：Windows 7/8/10 64-bit <br>
+处理器： Core i5-2500K 3.3 GHz / FX-6350 3.9 GHz <br>
+内存： 8 GB <br>
+显卡： 2 GB， GeForce GTX 660 / Radeon R7 260X， DirectX 11 <br>
+声卡： Windows compatible <br>
+可用空间： 60 GB <br>`,downloadUrl:"magnet:?xt=urn:btih:A31629656ECB6A1E52AF056BAC113D19B4303A5A&tr=http%3A%2F%2Fbt4.t-ru.org%2Fann%3Fmagnet&dn=Anno%201800%20-%20Complete%20Edition%20%5BP%5D%20%5BRUS%20%2B%20ENG%20%2B%209%20%2F%20RUS%20%2B%20ENG%5D%20(2019%2C%20RTS)%20(9.2%20%2B%209%20DLC)%20%5BP2P%5D"},{imgSrc:"/img/game/空洞骑士.jpg",title:"空洞骑士",description:`- 操作系统：Windows Seven / Eight / Ten <br>
+- 处理器：Intel Core 2 Duo E5200 2.4 GHz <br>
+- 内存：4.0 GB <br>
+- 显卡：NVidia GeForce 9800GTX+ <br>
+- 显存：1.0 GB <br>
+- DirectX：10 <br>
+- 可用硬盘空间：9.0 GB`,downloadUrl:"magnet:?xt=urn:btih:D5E7C4DC1C6E1A2A0CE47E2655F864EE0174F091&tr=http%3A%2F%2Fbt3.t-ru.org%2Fann%3Fmagnet&dn=%5BDL%5D%20Hollow%20Knight%20%5BL%5D%20%5BRUS%20%2B%20ENG%20%2B%208%5D%20(2017%2C%20Arcade)%20(1.5.78.11833%20%2B%202%20DLC)%20%5BGOG%5D"},{imgSrc:"/img/game/看门狗2.jpg",title:"看门狗2",description:`操作系统： Windows 7 SP1， Windows 8.1， Windows 10 （64bit versions only）<br>
+处理器： Intel Core i5 2400s @ 2.5 GHz， AMD FX 6120 @ 3.5 GHz or better<br>
+RAM： 6 GB RAM<br>
+显卡： NVIDIA GeForce GTX 660 with 2 GB VRAM or AMD Radeon HD 7870， with 2 GB VRAM or better<br>
+硬盘驱动器内存： 34 GB<br>
+声卡： DirectX compatible using the latest drivers<br>`,downloadUrl:"magnet:?xt=urn:btih:2DCB49D56AC693EBA4FE0CD5B3B9DA8FE1BB401E&tr=http%3A%2F%2Fbt3.t-ru.org%2Fann%3Fmagnet&dn=%5BDL%5D%20Watch%20Dogs%202%20%5BP%5D%20%5BRUS%20%2B%20ENG%20%2B%2015%20%2F%20RUS%20%2B%20ENG%5D%20(2016%2C%20TPS)%20(1.017.189.2%20%2B%2029%20DLC)%20%5BPortable%5D"},{imgSrc:"/img/game/哈迪斯.jpg",title:"哈迪斯",description:`- 需要 64 位处理器和操作系统<br>
+- 操作系统： Windows 7 （Service Pack 1）<br>
+- 处理器： Dual Core 3.0Ghz<br>
+- 内存： 4 GB RAM<br>
+- 显卡： 1Gb VRAM， DirectX 10+<br>
+- 存储空间： 需要 12 GB<br>`,downloadUrl:"magnet:?xt=urn:btih:5E9F6304AC0F91FDCF8148EB89264671ECE5A77A&tr=http%3A%2F%2Fbt2.t-ru.org%2Fann%3Fmagnet&dn=%5BDL%5D%20Hades%20%5BL%5D%20%5BRUS%20%2B%20ENG%20%2B%208%20%2F%20ENG%5D%20(2020%2C%20TPS)%20(1.38290%20%2B%201%20DLC)%20%5BEGS-Rip%5D"},{imgSrc:"/img/game/只狼-影度二逝.jpg",title:"只狼-影度二逝",description:`
+操作系统： Windows 7/8/10 64-bit  <br>
+处理器： Core i3-2100 / FX-6300 <br>
+记忆体： 4 GB <br>
+显卡： GeForce GTX 760 / Radeon HD 7950， DirectX 11 <br>
+声卡： DirectX 11 <br>
+可用空间： 13 GB <br>`,downloadUrl:"magnet:?xt=urn:btih:CFD1638934879602B01859D7398FA573E96AF29D&tr=http%3A%2F%2Fbt.t-ru.org%2Fann%3Fmagnet&dn=Sekiro%3A%20Shadows%20Die%20Twice%20-%20Game%20of%20the%20Year%20Edition%20%5BP%5D%20%5BRUS%20%2B%20ENG%20%2B%2011%20%2F%20ENG%20%2B%20JPN%5D%20(2019%2C%20TPS)%20(1.06)%20%5BP2P%5D"},{imgSrc:"/img/game/GTA5.jpg",title:"GTA5",description:`Windows 10 及更高<br>
+版本 处理器： Core i5-3470<br>
+内存： 8 GB<br>
+显卡： GeForce 660<br>
+硬盘：120 GB<br>`,downloadUrl:"magnet:?xt=urn:btih:57252C52EA9ED44E38D5DD39D7F5C5676AF183F3&tr=http%3A%2F%2Fbt4.t-ru.org%2Fann%3Fmagnet&dn=%5BDL%5D%20Grand%20Theft%20Auto%20V%20(5)%20%5BP%5D%20%5BRUS%20%2B%20ENG%20%2B%2011%20%2F%20ENG%5D%20(2015%2C%20TPS)%20(1.0.3095%20%2F%201.68)%20%5BPortable%5D"},{imgSrc:"/img/game/赛博朋克2077.jpg",title:"赛博朋克2077",description:`操作系统： Windows 10 64-bit<br>
+处理器： Core i5-3570K / FX-8310<br>
+记忆体： 8 GB<br>
+显卡： GeForce GTX 780 / Radeon RX 470， DirectX 12<br>
+声卡： OS compatible<br>
+可用空间： 92 GB（安装时~7 GB）<br>`,downloadUrl:"magnet:?xt=urn:btih:F2214606D20C621F9B6C45352113CAACA93CF5CE&tr=http%3A%2F%2Fbt4.t-ru.org%2Fann%3Fmagnet&dn=%5BDL%5D%20Cyberpunk%202077%20%5BP%5D%20%5BRUS%20%2B%20ENG%20%2B%2016%20%2F%20RUS%20%2B%20ENG%5D%20(2020%2C%20RPG)%20(2.12%20%2B%204%20DLC)%20%5BHD%20Reworked%20Project%2C%20Mod%5D"},{imgSrc:"/img/game/缺氧.jpg",title:"缺氧",description:`操作系统： Windows 7 （64 bit）<br>
+处理器： Dual Core 2 GHz<br>
+RAM： 4 GB RAM<br>
+显卡： Intel HD 4600 （AMD or NVIDIA equivalent）<br>
+DirectX： 版本 11<br>
+硬盘驱动器内存： 2 GB<br>`,downloadUrl:"magnet:?xt=urn:btih:B2A2E93E33CD07A77A468B09FF324198612E2294&tr=http%3A%2F%2Fbt2.t-ru.org%2Fann%3Fmagnet&dn=Oxygen%20Not%20Included%20%5BP%5D%20%5BRUS%20%2B%20ENG%20%2B%202%20%2F%20RUS%20%2B%20ENG%20%2B%202%5D%20(2019%2C%20Simulation)%20(490405%20%2B%201%20DLC)%20%5BScene%5D"},{imgSrc:"/img/game/消逝的光芒.jpg",title:"消逝的光芒",description:`- 需要 64 位处理器和操作系统<br>
+- 操作系统： Windows 7 （with the fix that is in the distribution） / 10<br>
+- 处理器： Intel Core i3-9100 / AMD Ryzen 3 2300X<br>
+- 内存： 8 GB RAM<br>
+- 显卡： NVIDIA GeForce GTX 1050 Ti / AMD Radeon RX 560 （4GB VRAM）<br>
+- 硬盘驱动器内存： 66 GB<br>`,downloadUrl:"magnet:?xt=urn:btih:D923F18B6BC1CE1FA908D27FA8CB3C9CAE7AA0FF&tr=http%3A%2F%2Fbt4.t-ru.org%2Fann%3Fmagnet&dn=Dying%20Light%202%3A%20Stay%20Human%20%5BP%5D%20%5BRUS%20%2B%20ENG%20%2F%20RUS%20%2B%20ENG%5D%20(2022%2C%20Horror)%20(1.15.2%20%2B%2027%20DLC)%20%5BPortable%5D"},{imgSrc:"/img/game/鬼泣5.jpg",title:"鬼泣5",description:`操作系统： Windows 7+ （x64）<br>
+处理器： Intel® Core™ i5-4460， AMD FX-6300™ or better<br>
+内存： 8 GB<br>
+显卡： NVIDIA® GeForce® GTX 760 or AMD Radeon™ R7 260x， 2GB video memory<br>
+硬盘驱动器内存： 41 GB<br>`,downloadUrl:"magnet:?xt=urn:btih:8E67DAD874DF5095AE8BC11D0E3164A3C3868C30&tr=http%3A%2F%2Fbt4.t-ru.org%2Fann%3Fmagnet&dn=Devil%20May%20Cry%205%20%5BP%5D%20%5BRUS%20%2B%20ENG%20%2B%2010%20%2F%20ENG%20%2B%20JPN%5D%20(2019%2C%20TPS)%20(update%2015.12.2020%20%2B%2031%20DLC)%20%5BScene%5D"},{imgSrc:"/img/game/刺客信条-英灵殿.jpg",title:"刺客信条-英灵殿",description:`操作系统： Windows 10 （64-bit）<br>
+处理器： AMD Ryzen R3 1200 / Intel Core i5-4460<br>
+内存： 8 GB<br>
+显卡： AMD Radeon R9 380 / nVidia GeForce GTX 960<br>
+存储空间： 145 GB 可用空间（安装期间为 +25 GB）<br>`,downloadUrl:"magnet:?xt=urn:btih:320EAD5371FCECD671F581DBCA622F78E4FEEF51&tr=http%3A%2F%2Fbt4.t-ru.org%2Fann%3Fmagnet&dn=Assassin%27s%20Creed%3A%20Valhalla.%20Complete%20Edition%20%5BP%5D%20%5BRUS%20%2B%20ENG%20%2B%204%20%2F%20RUS%20%2B%20ENG%5D%20(2020%2C%20TPS)%20(1.7.0)%20%5BP2P%5D"}],f=o=>(R("data-v-7502a3aa"),o=o(),S(),o),V={key:0,class:"popup"},W=f(()=>e("p",{class:"title"},[_(" 先决条件:种子下载器 点击 "),e("a",{href:"https://www.fosshub.com/qBittorrent.html?dwl=qbittorrent_4.6.5_x64_setup.exe#"},"下载")],-1)),H={__name:"Popup",setup(o){const n=d(!0);function t(){n.value=!1}return(i,a)=>n.value?(B(),l("div",V,[e("div",{class:"popup-content"},[e("button",{class:"close-button",onClick:t},"关闭"),W])])):h("",!0)}},j=p(H,[["__scopeId","data-v-7502a3aa"]]),y={class:"gameShower"},k={__name:"ToolsView",setup(o){const n=d(m),t=d(m);function i(){t.value=n.value}const a=r=>{if(r===null||r===""){i();return}t.value=[];const D=RegExp(`.*(${r}).*`,"g");for(let c of n.value){let b=c.title,g=D.test(b);if(console.log(D,b,"result:",g),g===!0){t.value.push(c);break}}};return(r,D)=>(B(),l("main",null,[F(j),e("div",y,[F(L,{onInputChange:a}),(B(!0),l(w,null,N(t.value,c=>(B(),P(x,{game:c},null,8,["game"]))),256))])]))}};export{k as default};
